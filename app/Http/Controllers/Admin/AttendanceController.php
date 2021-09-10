a5b972cb2e8ae0b058ab5ec3f6a4a36bace41d7d
iando24 <iandoraian00@gmail.com>
2021-09-10 15:40:46 +0300

    Attendence

--- app/Http/Controllers/Admin/AttendanceController.php
+++ app/Http/Controllers/Admin/AttendanceController.php
@@ -6,15 +6,10 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Http;
+use App\Models\Mdt;
 
 class AttendanceController extends Controller
 {
-
-    public function attendaceMDT(){
-
-
-
-    }
 
     /**
      * Display a listing of the resource.
@@ -23,27 +18,22 @@ class AttendanceController extends Controller
      */
     public function index()
     {
-	    // $title = 'Attendance';
-        // $mdtURL = "http://cmsv6.mobilevideosystems.net/api/v1/face/queryAttendRecord";
+	    $title = 'Attendance';
+        $mdtURL = "http://cmsv6.mobilevideosystems.net/api/v1/face/queryAttendRecord";
+        $mdtCountURL = "http://cmsv6.mobilevideosystems.net/api/v1/face/getAttendRecordCount";
 
-        // $response = Http::post($mdtURL,[
-        //     'startId' => 1,
-        //     'reqCount' => 200,
-        //     'needImg' => false
-        // ]);
-        // $data = array();
-        // $data = $response->body();
+        $response = Http::post($mdtURL,[
+            'startId' => 0,
+            'reqCount' => 908,
+            'needImg' => false
+        ]);
+        $data = array();
+        $data = $response->body();
 
-        // $json = json_decode($data);
+        $json = json_decode($data);
 
-        // $attadences = $json->data;
+        $attedences = $json->data;
 
-        // foreach($attadences as $attadence){
-        //     dd($attadence->personId);
-        // }
-
-            // ->with('attendances', $attadences)
-
-	    return view('admin.attendance.index',compact('title'));
+	    return view('admin.attendance.index',compact('title'))->with('attendances', $attedences);
     }
 }
--- resources/views/admin/attendance/index.blade.php
+++ resources/views/admin/attendance/index.blade.php
@@ -1,6 +1,8 @@
 @extends('admin.layouts.master')
 @section('title',$title)
 @section('content')
+<div class="container-fluid">
+
 <ul class="nav nav-pills nav-fill">
     <li class="nav-item">
       <a class="nav-link active" href="#">Student</a>
@@ -33,6 +35,18 @@
 
      </div>
      <div class="col-md-3">
+         <label for="class">COUNT:</label>
+        <select class=" class custom-select custom-select-lg mb-3">
+            <option selected>Select count</option>
+            <option value="1">10</option>
+            <option value="1">20</option>
+            <option value="1">30</option>
+            <option value="1">40</option>
+            <option value="1">50</option>
+        </select>
+
+     </div>
+     <div class="col-md-3">
          <label for="section">CLASS:</label>
         <select class="section custom-select custom-select-lg mb-3">
             <option selected>Select section</option>
@@ -58,28 +72,95 @@
          <button class="btn btn-success">DOWNLOAD</button>
      </div>
  </div>
- <table class="table table-hover">
-  <thead>
-    <tr>
-      <th scope="col">ID</th>
-      <th scope="col">USERNAME</th>
-      <th scope="col">DATE</th>
-      <th scope="col">IN TIME</th>
-      <th scope="col">OUT TIME</th>
-      <th scope="col">MASK</th>
-      <th scope="col">TEMPERATURE</th>
-    </tr>
-  </thead>
 
-</table>
-  <!-- @foreach($attendances as $attendance)
-    <p>{{ $attendance->bodyTemperature }}</p>
-  @endforeach -->
+ <table id="example" class="table table-striped table-bordered table-sm" cellspacing="0" width="100%">
+    <thead>
+        <tr>
+        <th scope="col">ID</th>
+        <th scope="col">USERNAME</th>
+        <th scope="col">DATE</th>
+        <th scope="col">IN TIME</th>
+        <th scope="col">OUT TIME</th>
+        <th scope="col">MASK</th>
+        <th scope="col">TEMPERATURE</th>
+        </tr>
+    </thead>
+
+    <tbody>
+            @php
+                foreach ($attendances as $attendance){
+
+            @endphp
+                <tr>
+                    <td>
+                        @php
+                            echo $attendance->id
+                        @endphp
+                    </td>
+                    <td>
+                        @php
+                            echo $attendance->name
+                        @endphp
+                    </td>
+                    <td>
+                        @php
+                            $time = (string) $attendance->timestamp;
+                            $date = "";
+                            $hours = "";
+                            for($i = 0; $i < strlen($time); $i++){
+                                if($i <= 10){
+                                    $date = $date.$time[$i];
+                                }else{
+                                    $hours = $hours.$time[$i];
+                                }
+                            }
+                            echo $date;
+                        @endphp
+                    </td>
+                    <td>
+                        @php
+                            echo $hours
+                        @endphp
+                    </td>
+                    <td></td>
+                    <td>
+                        @php
+                            if($attendance->respirator == 1){
+                                echo "With";
+                            }else{
+                                echo "whithout";
+                            }
+                        @endphp
+                    </td>
+                    <td>
+                        @php
+                            $temperat = (($attendance->bodyTemperature * 9)/5) + 32;
+                            echo $temperat;
+                        @endphp
+                    </td>
+                </tr>
+            @php
+                }
+            @endphp
+    </tbody>
+
+    </table>
+ </div>
+
 
 @endsection
 @section('stylesheets')
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.1/css/dataTables.bootstrap5.min.css">
 @endsection
 @section('scripts')
-
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.11.1/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.1/js/dataTables.bootstrap5.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#example').DataTable();
+        } );
+    </script>
 @endsection
